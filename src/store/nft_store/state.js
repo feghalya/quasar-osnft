02a8fdbb5471e5888d7893ac595458ec7d68e748
feghalya <albert.feghaly@intellart.ca>
2023-02-15 18:00:36 -0500

    Update team and weekly update

--- src/store/nft_store/state.js
+++ src/store/nft_store/state.js
@@ -2,9 +2,34 @@ export default function () {
   return {
     'weekly_update' : 
       [
-        { date: '2022-06-24', title: "Ongoing Plutus buy/sell implementation." },
-        { date: '2022-06-17', title: "Ongoing Plutus buy/sell implementation." },
-        { date: '2022-06-10', title: "Started Plutus buy/sell strategy." },
+        { date: '2022-12-16', title: "Veritheum: Now working on testnet." },
+        { date: '2022-12-09', title: "Veritheum: Connecting buy/sell on-chain functionality to off-chain DB." },
+        { date: '2022-12-02', title: "PubWeave design complete." },
+        { date: '2022-11-25', title: "Update Intellart's backend to Ruby on Rails 7.0." },
+        { date: '2022-11-18', title: "PubWeave: Bootstrapping workshop with Thespian (thespian.hr)." },
+        { date: '2022-11-11', title: "Plutus buy/sell smart contract scripts completed." },
+        { date: '2022-11-04', title: "Integrating plutus buy/sell contracts with frontend." },
+        { date: '2022-10-28', title: "PubWeave: Design brainstorming and planning." },
+        { date: '2022-10-21', title: "Debugging and finalizing pyCardano integration." },
+        { date: '2022-10-14', title: "Veritheum: Admin dashboard completed." },
+        { date: '2022-10-07', title: "Article #3 published: 'Blockchain in Science'." },
+        { date: '2022-09-27', title: "Success of PubWeave in Cardano's Catalyst Fund9 'Dapps, Products & Integrations' category!" },
+        { date: '2022-09-23', title: "Plutus smart contract scripts test modules with pyCardano." },
+        { date: '2022-09-16', title: "Plutus tests libraries exploration." },
+        { date: '2022-09-09', title: "Plutus smart contract scripts CLI testing." },
+        { date: '2022-09-02', title: "Implemented wallet connection to the Cardano blockchain." },
+        { date: '2022-08-26', title: "Finalized plutus buy/sell scripts." },
+        { date: '2022-08-19', title: "Nami wallet interface integrated to the frontend." },
+        { date: '2022-08-12', title: "Nami wallet integrated to the backend." },
+        { date: '2022-08-05', title: "Implemented using CLI for testing plutus scripts." },
+        { date: '2022-07-29', title: "Article #2 published: 'The Scientific Process in Academia'." },
+        { date: '2022-07-22', title: "Setting up the Plutus Application Backend (PAB)." },
+        { date: '2022-07-17', title: "Article #1 published: 'Academic Publishing Explained'." },
+        { date: '2022-07-08', title: "Local deployement of Cardano node for plutus script testing." },
+        { date: '2022-07-01', title: "Started implementing plutus script testing modules." },
+        { date: '2022-06-24', title: "Ongoing plutus buy/sell implementation." },
+        { date: '2022-06-17', title: "Ongoing plutus buy/sell implementation." },
+        { date: '2022-06-10', title: "Started plutus buy/sell strategy." },
         { date: '2022-06-03', title: "Frontend/Backend communication enhancements." },
         { date: '2022-05-27', title: "Backend: Exchange rate fetching through CoinGecko API integration." },
         { date: '2022-05-20', title: "Frontend/Backend websocket implementation." },
@@ -20,14 +45,14 @@ export default function () {
         { date: '2022-03-11', title: "UI: OpenScience Marketplace UI design finalization." },
         { date: '2022-03-04', title: "UI: OpenScience Marketplace UI design." },
         { date: '2022-02-25', title: "UI: OpenScience Marketplace design discussions and planification" },
-        { date: '2022-02-18', title: "New collaboration with Thespian Agency (https://thespian.hr)." },
+        { date: '2022-02-18', title: "New collaboration with Thespian Agency (https://thespian.hr) initiated." },
         { date: '2022-02-10', title: "Success of PanDAO in Cardano's Catalyst Fund7 'DAOs ❤  Cardano' category!" },
         { date: '2022-02-10', title: "First place in Cardano's Catalyst Fund7 'dApps and Integrations' category!" },
-        { date: '2022-02-04', title: "Ongoing plutus integration." },
-        { date: '2022-01-28', title: "Ongoing plutus integration." },
+        { date: '2022-02-04', title: "Ongoing blockchain and plutus script design strategies." },
+        { date: '2022-01-28', title: "Ongoing blockchain and plutus script design strategies." },
         { date: '2022-01-21', title: "Started exploration of generative osNFT creation." },
-        { date: '2022-01-14', title: "Started plutus integration." },
-        { date: '2022-01-07', title: "Model organisms design for osNFT frameworks." },
+        { date: '2022-01-14', title: "Started blockchain functionality design for the OpenScience NFT Marketplace." },
+        { date: '2022-01-07', title: "Started model organisms design for osNFT frameworks." },
         { date: '2021-12-31', title: "Year-wrap, priorities assessment and 2022 goals set." },
         { date: '2021-12-24', title: "Break, happy holidays!" },
         { date: '2021-12-17', title: "Begin smartweave contracts integration for NFT upload." },
@@ -65,32 +90,40 @@ export default function () {
           linkedin_url: 'https://www.linkedin.com/in/jordan-quenneville-64b18b114'
         },
         {
-          name: 'Jack Bauer',
-          title: 'Science Communicator',
-          photo: 'jack.png',
-          desc: "Last year of PhD in Developmental Biology.",
-          website_url: 'https://sciwalkart.bigcartel.com/',
-          linkedin_url: 'https://www.linkedin.com/in/jack-bauer-6b688999/',
-          instagram_url: 'https://www.instagram.com/sciwalkart/'
+          name: 'Ema Flores Diaz',
+          title: 'Science Communicator ',
+          photo: 'ema.png',
+          desc: "Last year of PhD in targeted therapies for leukemia.",
+          website_url: 'https://www.quierociencia.com/',
+          linkedin_url: 'https://www.linkedin.com/in/ema-flores-diaz-905204137',
         },
-        {
-          name: 'Yahya Benslimane, PhD',
-          title: 'Academic Coordinator',
-          photo: 'yahya.png',
-          desc: "Geneticist and CRISPR specialist.",
-          linkedin_url: 'https://www.linkedin.com/in/yahya-benslimane-71517898',
-        },
+        //{
+        //  name: 'Jack Bauer',
+        //  title: 'Science Communicator',
+        //  photo: 'jack.png',
+        //  desc: "Last year of PhD in Developmental Biology.",
+        //  website_url: 'https://sciwalkart.bigcartel.com/',
+        //  linkedin_url: 'https://www.linkedin.com/in/jack-bauer-6b688999/',
+        //  instagram_url: 'https://www.instagram.com/sciwalkart/'
+        //},
+        //{
+        //  name: 'Yahya Benslimane, PhD',
+        //  title: 'Academic Coordinator',
+        //  photo: 'yahya.png',
+        //  desc: "Geneticist and CRISPR specialist.",
+        //  linkedin_url: 'https://www.linkedin.com/in/yahya-benslimane-71517898',
+        //},
       ],
 
     'advisors':
       [
-        {
-          name: 'Roger Palou, PhD',
-          title: 'Academic advisor',
-          photo: 'roger.png',
-          desc: 'Microbiome Engineering Specialist @ Institute for Research in Immunology and Cancer (IRIC).',
-          linkedin_url: 'https://www.linkedin.com/in/roger-palou-phd-b963622a'
-        },
+        //{
+        //  name: 'Roger Palou, PhD',
+        //  title: 'Academic advisor',
+        //  photo: 'roger.png',
+        //  desc: 'Microbiome Engineering Specialist @ Institute for Research in Immunology and Cancer (IRIC).',
+        //  linkedin_url: 'https://www.linkedin.com/in/roger-palou-phd-b963622a'
+        //},
         {
           name: 'Tariq Daouda, PhD',
           title: 'Technological advisor',
@@ -99,13 +132,13 @@ export default function () {
           website_url: 'https://www.bluwr.com/',
           linkedin_url: 'https://www.linkedin.com/in/tariqdaouda'
         },
-        {
-          name: 'Maude Dumont-Lagacé, PhD',
-          title: 'Scientific advisor',
-          photo: 'maude.png',
-          desc: 'COO | CSO | Co-founder @ Piercing Star Technologies.',
-          linkedin_url: 'https://www.linkedin.com/in/maude-dumont-lagace'
-        }
+        //{
+        //  name: 'Maude Dumont-Lagacé, PhD',
+        //  title: 'Scientific advisor',
+        //  photo: 'maude.png',
+        //  desc: 'COO | CSO | Co-founder @ Piercing Star Technologies.',
+        //  linkedin_url: 'https://www.linkedin.com/in/maude-dumont-lagace'
+        //}
       ]
   }
 }
